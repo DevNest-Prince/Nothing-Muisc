57eda65485e60fef35c4331e7f2bdd268199333a
Prince Kothari <197196645+princekotharii@users.noreply.github.com>
2026-04-01 00:24:03 +0530

    feat: export BotCard component for improved bot display functionality

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -5,6 +5,7 @@ export { default as Footer } from './Footer';
 export { default as HeroSection } from './HeroSection';
 export { default as FeatureGrid } from './FeatureGrid';
 export { default as CommandCard } from './CommandCard';
+export { default as BotCard } from './BotCard';
 export { default as FAQItem } from './FAQItem';
 export { default as SectionHeader } from './SectionHeader';
 export { default as Button } from './Button';
